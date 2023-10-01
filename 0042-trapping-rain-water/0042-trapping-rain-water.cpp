452c1a641ba40604b859cc67c21ebf31231913de
ZaidMushtaq921 <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-10-02 00:19:23 +0530

    Time: 3 ms (99.47%), Space: 20.1 MB (53.46%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l=0;
+        int dif=0;
+        int sum=0;
+        for(int i=1;i<height.size();i++)
+        {
+            while(height[l]<=height[i])
+            {
+                  l=i;
+                i++;
+                if(i>=height.size())
+                    break;
+              
+            }
+                        if(i>=height.size())
+                    break;
+            dif=height[l]-height[i];
+            sum+=dif;
+        }
+        int m=height.size()-1;
+        for(int i=height.size()-1;i>l;i--)
+        {
+            if(height[i]>height[m])
+            {
+                m=i;
+            }
+            dif=height[l]-height[m];
+            sum-=dif;
+          
+        }
+        
+        
+        return sum;
+        
+        
+    }
+};
