77afc07592e16fe8e753ca71e3f8f1483ecf5458
ZaidMushtaq921 <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-10-02 22:26:35 +0530

    Time: 17 ms (41.71%), Space: 17.4 MB (24.61%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,6 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         
        int ans=0;
+        sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++)
         {
           ans^=nums[i];
