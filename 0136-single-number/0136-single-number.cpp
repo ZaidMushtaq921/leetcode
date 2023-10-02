d224b24fe54cd45fc4b8c6f72c8c313807c986a3
ZaidMushtaq921 <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-10-02 22:06:12 +0530

    Time: 22 ms (23.48%), Space: 17.2 MB (38.16%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -4,10 +4,15 @@ public:
         
   
         sort(nums.begin(),nums.end());
-              int ans=nums[nums.size()-1];
-        for(int i=0;i+1<nums.size();i=i+2)
+              int ans;
+        for(int i=0;i<nums.size();i=i+2)
         {
            
+              if(i==nums.size()-1)
+            {
+                ans= nums[i];
+                break; 
+            }
             if(nums[i]!=nums[i+1])
             {
                 ans= nums[i];
