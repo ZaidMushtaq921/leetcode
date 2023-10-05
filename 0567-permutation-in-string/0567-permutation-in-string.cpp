2deddef5beeb48d81d9965a57f385efa45618652
ZaidMushtaq921 <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-10-06 00:19:32 +0530

    Time: 2 ms (96.65%), Space: 7.7 MB (34.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,33 +1,48 @@
 class Solution {
-    bool areVectorsEqual(vector<int> a, vector<int> b){
+    bool areVectorsEqual(int a[], int  b[]){
         for(int i=0; i<26; i++){
-            if(a[i]!=b[i]) return false;
+            if(a[i]!=b[i])
+                return false;
         }
         return true;
     }
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26, 0);
-        for(char c: s1) freqS1[c-'a']++;
-        
-        vector<int> freqS2(26, 0);
-        int i=0, j=0;
-        
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;
-            
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1, freqS2)) return true;
-            }
-            
-            if(j-i+1<s1.size()) j++;
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
-            }
+        if(s2.length()<s1.length()) 
+            return false;
+        int count1[26]={0};
+        for(int i=0;i<s1.length();i++)
+        {
+            int index=s1[i]-'a';
+            count1[index]++;
         }
+       int i=0;
+        int window=s1.length();
+        int count2[26]={0};
+        
+        while(i<window)
+        {
+           int index=s2[i]-'a';
+            count2[index]++; 
+            i++;
+        }
+        if(areVectorsEqual(count1,count2))
+            return true;
+        while(i<s2.length())
+        {
+           
+            int index= s2[i]-'a';
+            count2[index]++;
+            
+             index = s2[i-window]-'a';
+            count2[index]--;
+           i++;
+             if(areVectorsEqual(count1,count2))
+            return true;
+            
+            
+        }
+  
         return false;
     }
 };
