2451df962a23052da71e015c9c2b9fabcfdf9b06
ZaidMushtaq921 <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-10-05 22:39:19 +0530

    Time: 6 ms (35.95%), Space: 7.1 MB (15.98%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -3,9 +3,7 @@ public:
     string removeOccurrences(string s, string part) {
        
       while(s.length()!=0 && s.find(part)<s.length())
-      {
           s.erase(s.find(part),part.length());
-      }
         return s;
     }   
 };
