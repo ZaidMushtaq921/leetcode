4b0caec50f0082ecee6f75aede6dcad7abe9df4b
ZaidMushtaq921 <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-10-06 22:40:56 +0530

    Time: 16 ms (71.31%), Space: 19.5 MB (37.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,11 +4,9 @@ public:
         int j = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                nums[j++] = nums[i];
+               swap(nums[j++],nums[i]);
             }
         }
-        while(j<nums.size()) {
-            nums[j++] = 0;
-        }
+
     }
 };
