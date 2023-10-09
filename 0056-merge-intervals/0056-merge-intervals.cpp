39034923d7af92f1d7289a0dc37de2d951b2fea9
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-10-10 01:03:39 +0530

    Time: 28 ms (72.74%), Space: 19.5 MB (30.34%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+     vector<vector<int>> ans;
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        int a = intervals[0][0];
+        int b = intervals[0][1];
+        for(int i=1;i<n;i++)
+        {
+         if(b>=intervals[i][0]&&b<intervals[i][1])
+         {
+             b=intervals[i][1];
+      
+         }
+            else if(b<intervals[i][0])
+            {
+                ans.push_back({a,b});
+                a=intervals[i][0];
+                b=intervals[i][1];
+            }
+            
+        }
+        ans.push_back({a,b});
+        return ans;
+        
+    }
+};
