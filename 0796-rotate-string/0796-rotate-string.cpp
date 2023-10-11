76e6525827958d9c4401f8a3f8c2e3642c79577e
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-10-12 00:12:31 +0530

    Time: 2 ms (49.40%), Space: 6.6 MB (24.25%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,16 +3,7 @@ public:
     bool rotateString(string s, string goal) {
       if(s.length()!=goal.length())
           return false;
-        // string check=s+s;
         return (s+s).find(goal)!=string::npos;
-        
     }
 };
-// class Solution {
-// public:
-//     bool rotateString(string s, string goal) {
-//         if(s.size() != goal.size()) return false;
-//         string check = s+s;
-//         return check.find(goal) != string::npos;
-//     }
-// };
+
