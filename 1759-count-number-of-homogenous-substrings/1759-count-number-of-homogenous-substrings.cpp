131039c6edd80c8302542423b115a5a052ae9714
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-11-09 13:27:21 +0530

    Time: 20 ms (93.70%), Space: 12 MB (85.65%) - LeetHub

---
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countHomogenous(string s) {
+        int sum = 0;
+        long long count = 1;
+        const int MOD = 1e9 + 7;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && s[i] == s[i + 1]) {
+                count++;
+            } else {
+                // Calculate the number of homogenous substrings and add it to the sum
+                sum = (sum + (count * (count + 1) / 2) % MOD) % MOD;
+                count = 1;
+            }
+        }
+
+        return sum;
+    }
+};
