3034799162c006731daee0c211b335370e980f59
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-11-11 23:38:57 +0530

    Time: 28 ms (76.64%), Space: 10.9 MB (65.75%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -3,27 +3,14 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int n = s.length();
-        unordered_set<char> uniqueChars;  // To keep track of unique characters
-        int count = 0;  // Count of substrings
-
-        for (int i = 0; i < n; ++i) {
-            if (uniqueChars.find(s[i]) == uniqueChars.end()) {
-                // Current character is unique, start a new substring
-                uniqueChars.insert(s[i]);
-            } else {
-                // Current character is not unique, end the current substring
-                count++;
-                uniqueChars.clear();  // Clear the set for the next substring
-                uniqueChars.insert(s[i]);  // Start a new substring with the current character
-            }
+        int cnt[26] = {}, res = 1;
+    for (int i = 0; i < s.size(); ++i) {
+        if (cnt[s[i] - 'a']) {
+            ++res;
+            fill(begin(cnt), end(cnt), 0);
         }
-
-        // If there are any remaining unique characters, count them as a separate substring
-        if (!uniqueChars.empty()) {
-            count++;
-        }
-
-        return count;
+        ++cnt[s[i] - 'a'];
+    }
+    return res; 
     }
 };
