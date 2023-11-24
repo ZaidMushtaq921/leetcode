5665f4c66ab87ce93042b083b73af17271939170
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-11-24 21:48:48 +0530

    Time: 43 ms (90.31%), Space: 19 MB (37.96%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,64 @@
+class node
+{
+    public:
+        int data;
+    int row;
+    int col;
+    node(int d, int r, int c)
+    {
+        data = d;
+        row = r;
+        col = c;
+    }
+};
+class compare
+{
+    public:
+        bool operator()(node *a, node *b)
+        {
+            return a->data > b->data;
+        }
+};
+
+class Solution
+{
+    public:
+        vector<int> smallestRange(vector<vector < int>> &nums)
+        {
+            int k = nums.size();
+            int mini = INT_MAX, maxi = INT_MIN;
+            priority_queue<node*, vector<node*>, compare> minheap;
+            for (int i = 0; i < k; i++)
+            {
+                int element = nums[i][0];
+                mini = min(mini, element);
+                maxi = max(maxi, element);
+                minheap.push(new node(element, i, 0));
+            }
+            int start = mini, end = maxi;
+            while (!minheap.empty())
+            {
+                node *temp = minheap.top();
+                minheap.pop();
+                mini = temp->data;
+                if (maxi - mini < end - start)
+                {
+                    start = mini;
+                    end = maxi;
+                }
+                if (temp->col + 1 < nums[temp->row].size())
+                {
+                    maxi = max(maxi, nums[temp->row][temp->col + 1]);
+                    minheap.push(new node(nums[temp->row][temp->col + 1], temp->row, temp->col + 1));
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return {
+                start,
+                end
+            };
+        }
+};
