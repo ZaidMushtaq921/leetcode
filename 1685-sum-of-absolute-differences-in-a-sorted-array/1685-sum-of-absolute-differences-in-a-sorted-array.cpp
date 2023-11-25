6ed02d5af0d7e068325a9daf8a8492721bdb3d92
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-11-26 00:22:21 +0530

    Time: 95 ms (48.11%), Space: 88 MB (28.65%) - LeetHub

---
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+            int total_sum= 0;
+            vector<int> result;
+        for(auto it: nums)
+        {
+                total_sum+=it;
+        }
+            
+          int prefix_sum=0; 
+            int suffix_sum=total_sum;
+            int size=nums.size();
+         for( int i=0;i<size;i++)
+         {
+                int val= abs(prefix_sum-(nums[i]*(i-0))) + abs(suffix_sum-(nums[i]*(size-i)));
+                 
+                 result.push_back(val);
+                 suffix_sum-=nums[i];
+               prefix_sum+=nums[i];  
+                 
+         }
+            return result;
+            
+    }
+};
