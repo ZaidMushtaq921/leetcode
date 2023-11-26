4b88694ea48b4d2e5bac31e9e62cfaf3eaf20eff
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-11-26 22:31:28 +0530

    Time: 95 ms (83.93%), Space: 58.4 MB (96.13%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-    int maxOperations(std::vector<int>& nums, int k) {
-        int count = 0;
-        std::unordered_map<int, int> mp;
-
-        // Count the frequency of each element in the nums vector
-        for (auto it : nums) {
-            mp[it]++;
-        }
-
-        // Iterate through the nums vector
-        for (int i = 0; i < nums.size(); i++) {
-               if (mp[nums[i]] == 0) {
-                    mp.erase(nums[i]);
-                       continue;
-                }
-            int left = k - nums[i];
- mp[nums[i]]--;
-            // Check if the complement element is present in the map
-            if (mp.count(left) > 0 && mp[left] > 0) {
-                count++;
-                mp[left]--;
-
-               
-                // If the frequency becomes 0, remove the element from the map
-                if (mp[left] == 0) {
-                    mp.erase(left);
-                }
-
-                // Decrement the frequency of the current element
-                
-
-                // If the frequency becomes 0, remove the element from the map
-                if (mp[nums[i]] == 0) {
-                    mp.erase(nums[i]);
-                }
+    int maxOperations(vector<int>& nums, int k) {
+        
+            sort(begin(nums),end(nums));
+            int i=0;
+            int j=nums.size()-1;
+            int count=0;
+            while(i<j)
+            {
+                    int sum=nums[i]+nums[j];
+                    if(sum<k)
+                    {
+                           i++;
+                    }
+                    else if(sum>k)
+                    {
+                            j--;
+                            
+                    }else
+                    {
+                            count++;
+                            i++;
+                            j--;
+                    } 
             }
-        }
-        return count;
+            return count;
+            
     }
 };
