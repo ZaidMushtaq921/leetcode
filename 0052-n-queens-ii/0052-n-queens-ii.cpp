ddc63e19edee462989374a8cad8575f32e460eb2
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-11-27 22:08:45 +0530

    Time: 5 ms (48.55%), Space: 6.5 MB (69.74%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,39 @@
+
+class Solution {
+public:
+    int totalNQueens(int n) {
+        int count = 0;
+        vector<string> board(n, string(n, '.'));
+
+        solveNQueensHelper(board, 0, count);
+
+        return count;
+    }
+
+private:
+    void solveNQueensHelper(vector<string>& board, int row, int& count) {
+        if (row == board.size()) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < board.size(); ++col) {
+            if (is_valid(board, row, col)) {
+                board[row][col] = 'Q';
+                solveNQueensHelper(board, row + 1, count);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    bool is_valid(const vector<string>& board, int row, int col) const {
+        for (int i = 0; i < row; ++i) {
+            if (board[i][col] == 'Q' || 
+                (col - (row - i) >= 0 && board[i][col - (row - i)] == 'Q') || 
+                (col + (row - i) < board.size() && board[i][col + (row - i)] == 'Q')) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
