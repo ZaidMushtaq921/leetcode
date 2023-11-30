f6add1af8c3cccf0cd3abd7856c05ebf1456ac4c
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-11-30 23:12:35 +0530

    Time: 6 ms (93.06%), Space: 11.1 MB (69.65%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,35 +1,32 @@
 class Solution
 {
     public:
-        void dfs(int node, vector<vector < int>> &graph, vector< int > ans, vector< vector<                 int>> &result, unordered_map<int,bool> visited)
+        void dfs(int node, vector<vector < int>> &graph, vector< int > &ans, vector< vector< int >> &result, unordered_map< int, bool>& visited)
         {
-                
-                visited[node]=true;
-                ans.push_back(node);
-                 if(node==graph.size()-1)
-                                {
-                                        result.push_back(ans);
-                                }
-                for(auto neighbor: graph[node])
+
+            visited[node] = true;
+            ans.push_back(node);
+            if (node == graph.size() - 1)
+            {
+                result.push_back(ans);
+            }
+            for (auto neighbor: graph[node])
+            {
+                if (!visited[neighbor])
                 {
-                        if(!visited[neighbor])
-                        {
-                                dfs(neighbor,graph,ans,result,visited);
-                               
-                        }
-                     
+                    dfs(neighbor, graph, ans, result, visited);
                 }
-                // ans.pop_back();
-                // visited[node]=false;
-                       
+            }
+           	ans.pop_back();
+           	visited[node]=false;
         }
 
     vector<vector < int>> allPathsSourceTarget(vector<vector < int>> &graph)
     {
-            unordered_map<int,bool> visited;
+        unordered_map<int, bool> visited;
         vector<int> ans;
         vector<vector < int>> result;
-            dfs(0,graph,ans,result,visited);
-            return result;
+        dfs(0, graph, ans, result, visited);
+        return result;
     }
 };
