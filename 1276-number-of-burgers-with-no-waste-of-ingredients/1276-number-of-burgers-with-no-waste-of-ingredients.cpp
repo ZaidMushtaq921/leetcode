b1698d81e9a7132aded5808c63b83904b7979ab1
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-01 15:00:33 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (91.35%) - LeetHub

--- 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.cpp
+++ 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.cpp
@@ -1,30 +1,20 @@
+
 class Solution {
 public:
-    vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
-           int total_jumbo=0;
-            int total_small=0;
-            if(cheeseSlices==0 && tomatoSlices==0)
-                    return {total_jumbo,total_small};
-            if(tomatoSlices%2!=0 ||cheeseSlices==0 || tomatoSlices==0)
-                    return {};
-            while(tomatoSlices!=0&&cheeseSlices!=0)
-            {
-                    if(tomatoSlices/2==cheeseSlices)
-                    {
-                            total_small+=cheeseSlices;
-                            tomatoSlices=0;
-                            cheeseSlices=0;
-                            
-                    }
-                    else
-                    {
-                            total_jumbo+=1;
-                       tomatoSlices-=4;
-                            cheeseSlices-=1;     
-                    }
-            }
-            if(tomatoSlices==0&&cheeseSlices==0)
-            return {total_jumbo,total_small};
+    std::vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+        int jCount = 0, // x
+            sCount = 0; // y
+
+        // 4x + 2y = tomatoSlices
+        // x + y = cheeseSlices
+
+        if ((4 * cheeseSlices - tomatoSlices) < 0 || (4 * cheeseSlices - tomatoSlices) % 2 != 0) {
             return {};
+        }
+
+        sCount = (4 * cheeseSlices - tomatoSlices) / 2;
+        jCount = cheeseSlices - sCount;
+
+        return jCount >= 0 ? std::vector<int>{jCount, sCount} : std::vector<int>{};
     }
 };
