e0bb4f3f988abcdfd7a9b01f4f3a84f591e4eaaf
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-07 23:25:45 +0530

    Time: 9 ms (59.38%), Space: 13 MB (85.65%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,28 +1,22 @@
+#include <vector>
+#include <unordered_map>
+
 class Solution {
 public:
-    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-       
-            unordered_map<int,list<int>> m;
-            for(int i=0;i<groupSizes.size();i++)
-            {
-                    m[groupSizes[i]].push_back(i);
+    std::vector<std::vector<int>> groupThePeople(std::vector<int>& groupSizes) {
+        std::unordered_map<int, std::vector<int>> groups;
+        std::vector<std::vector<int>> result;
+
+        for (int i = 0; i < groupSizes.size(); ++i) {
+            int size = groupSizes[i];
+            groups[size].push_back(i);
+
+            if (groups[size].size() == size) {
+                result.push_back(groups[size]);
+                groups[size].clear();  // Clear the group for the next set
             }
-            vector<int> ans;
-            vector<vector<int>> result;
-            for(auto it:m)
-            {
-                    for(auto i:it.second)
-                    {
-                             ans.push_back(i);   
-                            if(ans.size()==it.first)
-                            {
-                                    result.push_back(ans);
-                                    ans.clear();
-                            }
-                                   
-                    }
-            }
-            
-return result; 
+        }
+
+        return result;
     }
 };
