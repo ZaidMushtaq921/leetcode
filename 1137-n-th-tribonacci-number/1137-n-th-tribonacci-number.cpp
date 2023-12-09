e501af9e81cd36217a04038456198339d881cafd
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-09 12:39:27 +0530

    Time: 2 ms (47.43%), Space: 6.4 MB (44.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,20 +1,18 @@
+#include <vector>
+
 class Solution {
 public:
     int tribonacci(int n) {
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
 
-        int t0 = 0;
-        int t1 = 1;
-        int t2 = 1;
+        vector<int> dp(n + 1, 0);
+        dp[1] = dp[2] = 1;
 
         for (int i = 3; i <= n; i++) {
-            int curr = t0 + t1 + t2;
-            t0 = t1;
-            t1 = t2;
-            t2 = curr;
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
 
-        return t2;
+        return dp[n];
     }
 };
