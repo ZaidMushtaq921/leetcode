d16f669f36806ffda329019ddf5b902a4712e550
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-09 11:12:55 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (37.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -18,15 +18,9 @@ public:
       }
         
     int rob(vector<int>& nums) {
-        vector<int> temp1,temp2;
             int n=nums.size();
             if(n==1)return nums[0];
-            for(int i=0;i<n;i++)
-            {
-                    if(i!=0)temp1.push_back(nums[i]);
-                    if(i!=n)temp2.push_back(nums[i]);     
-            }            
-            
-         return max(sum(temp1),sum(temp2));   
+     
+         return sum(nums);   
     }
 };
