f82a9c63a92747e041ec15eba3480eab25fa22c9
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-09 23:48:59 +0530

    Time: 12 ms (63.59%), Space: 11 MB (59.64%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,38 +1,23 @@
-class Solution
-{
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        vector<int> prev(m, 0);
 
-    public:
-        int minFallingPathSum(vector<vector < int>> &matrix)
-        {
-
-            int m = matrix.size();
-            vector<int> prev(m, 0);
-            for (int i = 0; i < m; i++)
-            {
-                prev[i] = matrix[0][i];
-            }
-            for (int i = 1; i < m; i++)
-            {
-                vector<int> curr(m, 0);
-                for (int j = 0; j < m; j++)
-                {
-                    int up = INT_MAX;
-                    int left = INT_MAX;
-                    int right = INT_MAX;
-                    up = matrix[i][j] + prev[j];
-                    if (j - 1 >= 0) left = matrix[i][j] + prev[j - 1];
-                    if (j + 1 < m) right = matrix[i][j] + prev[j + 1];
-
-                    curr[j] = min(up, min(right, left));
-                }
-                prev = curr;
-            }
-
-            int mini = prev[0];
-            for (int i = 1; i < m; i++)
-            {
-                mini = min(prev[i], mini);
-            }
-            return mini;
+        for (int i = 0; i < m; i++) {
+            prev[i] = matrix[0][i];
         }
+
+        for (int i = 1; i < m; i++) {
+            vector<int> curr(m, INT_MAX);
+            for (int j = 0; j < m; j++) {
+                curr[j] = matrix[i][j] + prev[j];
+                if (j - 1 >= 0) curr[j] = min(curr[j], matrix[i][j] + prev[j - 1]);
+                if (j + 1 < m) curr[j] = min(curr[j], matrix[i][j] + prev[j + 1]);
+            }
+            prev = curr;
+        }
+
+        return *min_element(prev.begin(), prev.end());
+    }
 };
