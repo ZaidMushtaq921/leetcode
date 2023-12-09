e1c247f6fde82705496e6cfc495f8f4c99fb8eb0
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-09 22:36:31 +0530

    Time: 5 ms (42.79%), Space: 9 MB (82.21%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,20 @@
+
 class Solution
 {
     public:
         int minimumTotal(vector<vector < int>> &triangle)
         {
             int m = triangle.size();
-            vector<vector < int>> dp(m, vector<int> (m, 0));
-            for (int i = m - 1; i >= 0; i--)
+            vector<int> dp(triangle.back());
+
+            for (int i = m - 2; i >= 0; --i)
             {
-                for (int j = i; j >= 0; j--)
+                for (int j = 0; j <= i; ++j)
                 {
-                    if (i == m - 1) dp[i][j] = triangle[i][j];
-                    else
-                    {
-                        int down = dp[i + 1][j];
-                        int diagonal = dp[i + 1][j + 1];
-                        dp[i][j] = min(down, diagonal) + triangle[i][j];
-                    }
+                    dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j];
                 }
             }
 
-            return dp[0][0];
+            return dp[0];
         }
 };
