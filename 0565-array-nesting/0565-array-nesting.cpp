686e5122048132db0acb510f9c41ee09829c9b8c
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-11 22:51:37 +0530

    Time: 137 ms (23.40%), Space: 123.7 MB (16.33%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.cpp
+++ 0565-array-nesting/0565-array-nesting.cpp
@@ -1,27 +1,22 @@
 class Solution {
-        int dfs(int ind,vector<int>& nums,vector<bool> &visited, int n)
-        {
-                if(ind>=n) return 0;
-                if( visited[ind]==true) return 0;
-                visited[ind]=true;
-                return 1+dfs(nums[ind],nums,visited,n);
-        }
-        
-        
+    int dfs(int ind, vector<int>& nums, vector<bool>& visited, int n) {
+        if (visited[ind]) return 0;
+        visited[ind] = true;
+        return 1 + dfs(nums[ind], nums, visited, n);
+    }
+
 public:
     int arrayNesting(vector<int>& nums) {
-            int n=nums.size();
-       vector<bool> visited(n,false);
-            int maxi=0;
-            int count=0;
-            for(int i=0;i<n;i++)
-            {
-               if(!visited[i])
-               {
-                       count=dfs(i,nums,visited,n);
-               }
-                    maxi=max(maxi,count);
+        int n = nums.size();
+        vector<bool> visited(n, false);
+        int maxi = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                maxi = max(maxi, dfs(i, nums, visited, n));
             }
-            return maxi;
+        }
+
+        return maxi;
     }
 };
