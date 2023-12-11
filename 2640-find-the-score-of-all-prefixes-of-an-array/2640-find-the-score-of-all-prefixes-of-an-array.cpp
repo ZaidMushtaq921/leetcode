7648364a10bc08cde1c76d197a916fc36a5a35eb
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-11 23:38:37 +0530

    Time: 82 ms (70.48%), Space: 57.5 MB (50.94%) - LeetHub

--- 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.cpp
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.cpp
@@ -1,15 +1,17 @@
-class Solution {
-public:
-    vector<long long> findPrefixScore(vector<int>& nums) {
-        vector<long long> result;
-            long long prefix=0;
-            int maxi=INT_MIN;
-            for(int i=0;i<nums.size();i++)
+class Solution
+{
+    public:
+        vector < long long > findPrefixScore(vector<int> &nums)
+        {
+            vector < long long > result;
+            long long prefix = 0;
+            int maxi = INT_MIN;
+            for (int i = 0; i < nums.size(); i++)
             {
-                    maxi=max(maxi,nums[i]);
-                    prefix+=nums[i] + maxi;
-                    result.push_back(prefix);  
+                maxi = max(maxi, nums[i]);
+                prefix += nums[i] + maxi;
+                result.push_back(prefix);
             }
             return result;
-    }
+        }
 };
