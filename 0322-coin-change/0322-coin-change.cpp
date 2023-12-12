146da6e233367aedd5e786680b44084a456973a8
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-12 17:51:41 +0530

    Time: 89 ms (35.43%), Space: 46.2 MB (20.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,15 +4,15 @@ class Solution
     {
         if (ind == 0)
         {
-            if (target%coins[ind] == 0) return target/coins[ind] ;
+            if (target % coins[ind] == 0) return target / coins[ind];
             return 1e9;
         }
-if(dp[ind][target]!=-1)return dp[ind][target];
+        if (dp[ind][target] != -1) return dp[ind][target];
         int nontake = f(ind - 1, target, coins, dp);
         int take = INT_MAX;
         if (coins[ind] <= target)
             take = 1 + f(ind, target - coins[ind], coins, dp);
-        return dp[ind][target]=min(take, nontake);
+        return dp[ind][target] = min(take, nontake);
     }
 
     public:
@@ -22,7 +22,7 @@ if(dp[ind][target]!=-1)return dp[ind][target];
             vector<vector < int>> dp(s, vector<int> (amount + 1, -1));
 
             int ans = f(s - 1, amount, coins, dp);
-            if (ans>=1e9)
+            if (ans >= 1e9)
                 return -1;
             return ans;
         }
