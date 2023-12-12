d8632d7a06f74b902b179a0f7111bca21699bf48
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-12 18:09:43 +0530

    Time: 108 ms (22.34%), Space: 45.5 MB (30.38%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,54 @@
 class Solution
 {
-    int f(int ind, int target, vector<int> &coins, vector<vector< int>> &dp)
+    // int f(int ind, int target, vector<int> &coins, vector<vector< int>> &dp)
+    // {
+    //     if (ind == 0)
+    //     {
+    //         if (target % coins[ind] == 0) return target / coins[ind];
+    //         return 1e9;
+    //     }
+    //     if (dp[ind][target] != -1) return dp[ind][target];
+    //     int nontake = f(ind - 1, target, coins, dp);
+    //     int take = INT_MAX;
+    //     if (coins[ind] <= target)
+    //         take = 1 + f(ind, target - coins[ind], coins, dp);
+    //     return dp[ind][target] = min(take, nontake);
+    // }
+
+   public:
+    int coinChange(vector<int> &coins, int target)
     {
-        if (ind == 0)
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+
+        for (int t = 0; t <= target; t++)
         {
-            if (target % coins[ind] == 0) return target / coins[ind];
-            return 1e9;
+            if (t % coins[0] == 0)
+                dp[0][t] = t / coins[0];
+            else
+                dp[0][t] = 1e9;
         }
-        if (dp[ind][target] != -1) return dp[ind][target];
-        int nontake = f(ind - 1, target, coins, dp);
-        int take = INT_MAX;
-        if (coins[ind] <= target)
-            take = 1 + f(ind, target - coins[ind], coins, dp);
-        return dp[ind][target] = min(take, nontake);
+
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int tar = 0; tar <= target; tar++)
+            {
+                int nontake = dp[ind - 1][tar];
+                int take = 1e9; // Initialize take with a large value
+
+                if (coins[ind] <= tar)
+                    take = 1 + dp[ind][tar - coins[ind]];
+
+                // Check if take is valid (not equal to INT_MAX)
+                if (take < 1e9)
+                    dp[ind][tar] = min(take, nontake);
+                else
+                    dp[ind][tar] = nontake;
+            }
+        }
+
+        if (dp[n - 1][target] >= 1e9)
+            return -1;
+        return dp[n - 1][target];
     }
-
-    public:
-        int coinChange(vector<int> &coins, int amount)
-        {
-            int s = coins.size();
-            vector<vector < int>> dp(s, vector<int> (amount + 1, -1));
-
-            int ans = f(s - 1, amount, coins, dp);
-            if (ans >= 1e9)
-                return -1;
-            return ans;
-        }
 };
