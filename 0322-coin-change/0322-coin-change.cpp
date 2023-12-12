1c8c5fd022742749c861897bf2ee2cee214db156
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-12 18:11:57 +0530

    Time: 40 ms (94.73%), Space: 14.7 MB (75.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,54 +1,24 @@
+#include <vector>
+#include <climits>
+
 class Solution
 {
-    // int f(int ind, int target, vector<int> &coins, vector<vector< int>> &dp)
-    // {
-    //     if (ind == 0)
-    //     {
-    //         if (target % coins[ind] == 0) return target / coins[ind];
-    //         return 1e9;
-    //     }
-    //     if (dp[ind][target] != -1) return dp[ind][target];
-    //     int nontake = f(ind - 1, target, coins, dp);
-    //     int take = INT_MAX;
-    //     if (coins[ind] <= target)
-    //         take = 1 + f(ind, target - coins[ind], coins, dp);
-    //     return dp[ind][target] = min(take, nontake);
-    // }
-
-   public:
+public:
     int coinChange(vector<int> &coins, int target)
     {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        vector<int> dp(target + 1, INT_MAX);
+        dp[0] = 0;
 
-        for (int t = 0; t <= target; t++)
+        for (int coin : coins)
         {
-            if (t % coins[0] == 0)
-                dp[0][t] = t / coins[0];
-            else
-                dp[0][t] = 1e9;
-        }
-
-        for (int ind = 1; ind < n; ind++)
-        {
-            for (int tar = 0; tar <= target; tar++)
+            for (int t = coin; t <= target; t++)
             {
-                int nontake = dp[ind - 1][tar];
-                int take = 1e9; // Initialize take with a large value
-
-                if (coins[ind] <= tar)
-                    take = 1 + dp[ind][tar - coins[ind]];
-
-                // Check if take is valid (not equal to INT_MAX)
-                if (take < 1e9)
-                    dp[ind][tar] = min(take, nontake);
-                else
-                    dp[ind][tar] = nontake;
+                if (dp[t - coin] != INT_MAX)
+                    dp[t] = min(dp[t], 1 + dp[t - coin]);
             }
         }
 
-        if (dp[n - 1][target] >= 1e9)
-            return -1;
-        return dp[n - 1][target];
+        return dp[target] == INT_MAX ? -1 : dp[target];
     }
 };
