878447487472892fabe4613a0f48767daf75c042
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-13 22:06:43 +0530

    Time: 19 ms (37.89%), Space: 13.2 MB (46.58%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,41 +1,37 @@
+#include <vector>
+
 class Solution
 {
-    public:
-        int numSpecial(vector<vector < int>> &mat)
-        {
-            int r = mat.size();
-            int c = mat[0].size();
-            vector<int> rowsum, colsum;
-            for (int i = 0; i < r; i++)
-            {
-                int sum = 0;
-                for (int j = 0; j < c; j++)
-                {
-                    sum += mat[i][j];
-                }
-                rowsum.push_back(sum);
-            }
-            for (int i = 0; i < c; i++)
-            {
-                int sum = 0;
-                for (int j = 0; j < r; j++)
-                {
-                    sum += mat[j][i];
-                }
-                colsum.push_back(sum);
-            }
-            int count = 0;
-            for (int i = 0; i < r; i++)
-            {
+public:
+    int numSpecial(vector<vector<int>> &mat)
+    {
+        int r = mat.size();
+        int c = mat[0].size();
+        vector<int> rowsum(r, 0);
+        vector<int> colsum(c, 0);
 
-                for (int j = 0; j < c; j++) {
-                    if(mat[i][j]==1)
-                    {
-                        if(rowsum[i]==1 && colsum[j]==1)
-                        count++;
-                    }
+        // Calculate row and column sums simultaneously
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                rowsum[i] += mat[i][j];
+                colsum[j] += mat[i][j];
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (mat[i][j] == 1 && rowsum[i] == 1 && colsum[j] == 1)
+                {
+                    count++;
                 }
             }
-            return count;
         }
+
+        return count;
+    }
 };
