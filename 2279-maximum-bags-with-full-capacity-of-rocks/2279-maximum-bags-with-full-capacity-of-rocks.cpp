71c1fc174e1cb0f441a74fbc5f0f01092c57d031
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-14 23:01:28 +0530

    Time: 185 ms (23.79%), Space: 94.5 MB (20.69%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,31 +1,33 @@
-class Solution
-{
-    public:
-    static    bool comp(pair<int, int> a, pair<int, int> b)
-        {
-            return a.first - a.second < b.first - b.second;
-        }
-    int maximumBags(vector<int> &capacity, vector<int> &rocks, int additionalRocks)
-    {
-        vector<pair<int, int>> p;
-            int count=0;
-        for (int i = 0; i < rocks.size(); i++)
-        {
-            p.push_back(make_pair(capacity[i], rocks[i]));
-        }
-        sort(p.begin(), p.end(), comp);
-        for (int i = 0; i < p.size(); i++)
-        {
-            pair<int, int> temp = p[i];
-            if (temp.first - temp.second == 0)
-                count++;
-            else if (additionalRocks >= temp.first - temp.second)
-            {
 
-                additionalRocks -= temp.first - temp.second;
+using namespace std;
+
+class Solution {
+public:
+    static bool comp(pair<int, int> a, pair<int, int> b) {
+        return a.first - a.second < b.first - b.second;
+    }
+
+    int maximumBags(vector<int> &capacity, vector<int> &rocks, int additionalRocks) {
+        int count = 0;
+        vector<pair<int, int>> p;
+
+        for (int i = 0; i < rocks.size(); i++) {
+            p.emplace_back(capacity[i], rocks[i]);
+        }
+
+        sort(p.begin(), p.end(), comp);
+
+        for (const auto& [c, r] : p) {
+            if (c == r) {
                 count++;
+            } else if (additionalRocks >= c - r) {
+                additionalRocks -= c - r;
+                count++;
+            } else {
+                break; // Stop checking once additionalRocks are not enough
             }
         }
+
         return count;
     }
 };
