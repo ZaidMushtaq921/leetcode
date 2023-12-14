a88ea681b87a2156a2435c7b3f1a371c78eb33c5
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-14 22:35:14 +0530

    Time: 89 ms (36.64%), Space: 6.5 MB (24.29%) - LeetHub

--- 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.cpp
@@ -9,20 +9,12 @@ public:
         pq.push(c);
 
         int count = 0;
+  while (pq.size() >= 2) {
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
 
-        while (pq.size() >= 2) {
-            int x = pq.top();
-            pq.pop();
-            int y = pq.top();
-            pq.pop();
-
-            if (x - 1 > 0) {
-                pq.push(x - 1);
-            }
-
-            if (y - 1 > 0) {
-                pq.push(y - 1);
-            }
+            if (--x > 0) pq.push(x);
+            if (--y > 0) pq.push(y);
 
             count++;
         }
