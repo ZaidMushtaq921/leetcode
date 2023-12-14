419cac3a58cf9783597341fc256affca85ee9374
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-14 21:53:16 +0530

    Time: 104 ms (10.99%), Space: 56.8 MB (56.28%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -1,27 +1,28 @@
-class Solution {
-public:
-    int maximumGroups(vector<int>& grades) {
-            int prevsum=0;
-            int prevcount=0;
-            sort(grades.begin(),grades.end());
-            int nextsum=0;
-            int nextcount=0;
-            int count=0;
-          for(int i=0;i<grades.size();i++)
-          {
-                
-                  nextsum+=grades[i];
-                  nextcount++; 
-                    if(prevsum<nextsum && prevcount<nextcount)
-                  {
-                          count++;
-                          prevsum=nextsum;
-                             prevcount=nextcount;
-                          nextsum=0;
-                          nextcount=0;
-                       
-                  }
-          }
-      return count;
-    }
+class Solution
+{
+    public:
+        int maximumGroups(vector<int> &grades)
+        {
+            int prevsum = 0;
+            int prevcount = 0;
+            sort(grades.begin(), grades.end());
+            int nextsum = 0;
+            int nextcount = 0;
+            int count = 0;
+            for (int i = 0; i < grades.size(); i++)
+            {
+
+                nextsum += grades[i];
+                nextcount++;
+                if (prevsum < nextsum && prevcount < nextcount)
+                {
+                    count++;
+                    prevsum = nextsum;
+                    prevcount = nextcount;
+                    nextsum = 0;
+                    nextcount = 0;
+                }
+            }
+            return count;
+        }
 };
