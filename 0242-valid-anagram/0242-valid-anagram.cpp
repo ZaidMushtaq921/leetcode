f13398f754ef433690da9c24ee9190c8dd55dd4e
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2023-12-16 17:32:33 +0530

    Time: 3 ms (97.78%), Space: 7.7 MB (45.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,16 +4,16 @@ public:
         if (s.size() != t.size())
             return false;
 
-        vector<int> sm(26, 0);
-        vector<int> tm(26, 0);
+        vector<int> counter(26, 0);
+      
 
         for (int i = 0; i < s.size(); i++) {
-            sm[s[i] - 'a']++;
-            tm[t[i] - 'a']++;
+           counter[s[i] - 'a']++;
+            counter[t[i] - 'a']--;
         }
 
         for (int i = 0; i < 26; i++) {
-            if (sm[i] != tm[i])
+            if (counter[i]!=0)
                 return false;
         }
 
