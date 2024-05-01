c46fe188255c62e79324ac4eae202d3297b08dfb
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2024-05-01 19:24:04 +0530

    Time: 17 ms (61.88%) | Memory: 33.1 MB (62.18%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -3,15 +3,19 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> result;
         
-        for(int i =0; i< words.size();i++)
-        {
-            size_t found = words[i].find(x);
-            if(found != string::npos)
-            {
+        for (int i = 0; i < words.size(); ++i) {
+            bool found = false;
+            for (char c : words[i]) {
+                if (c == x) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
                 result.push_back(i);
             }
         }
-        return result;
         
+        return result;
     }
 };
