807142f21ba712fc3b8fba21c6a4d1bd021159b1
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2024-05-01 11:58:36 +0530

    Time: 28 ms (13.78%) | Memory: 38.1 MB (19.49%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,19 +1,13 @@
-#include <vector>
-
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> result(n, 1);
-
-        // Calculate products of elements to the left of each element
         int product = 1;
         for (int i = 0; i < n; i++) {
             result[i] = product;
             product *= nums[i];
         }
-
-        // Calculate products of elements to the right of each element and multiply it with the corresponding product from the first pass
         product = 1;
         for (int i = n - 1; i >= 0; i--) {
             result[i] *= product;
