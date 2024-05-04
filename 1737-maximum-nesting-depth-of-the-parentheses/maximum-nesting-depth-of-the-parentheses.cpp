c84dc91c8f777afef57a2886f27d984d17469717
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2024-05-04 07:10:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (21.91%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-    int maxDepth(string s) {
-        int max=0;
-        int counter=0;
-        stack<char> stk;
-        for(auto c:s)
-        {
-            if(c=='(' || c==')')
-            {
-                stk.push(c);
-            }
-        }
-        while(!stk.empty()){
-            char c = stk.top();
-            stk.pop();
-            if(c==')')
-            {
+      int maxDepth(string s) {
+        int maxDepth = 0;
+        int counter = 0;
+        
+        for (char c : s) {
+            if (c == '(') {
                 counter++;
-                if(max<counter)
-                    max=counter;
-            }
-            else{
+                maxDepth = max(maxDepth, counter);
+            } else if (c == ')') {
                 counter--;
             }
         }
-        return max;
+        
+        return maxDepth;
     }
 };
