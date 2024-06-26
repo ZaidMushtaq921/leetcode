f9b8d52b3971df480f811f6312f253dd4e0b6714
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2024-06-26 22:09:33 +0530

    Time: 2 ms (77.57%) | Memory: 11.4 MB (30.28%) - LeetSync

---
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int result=0;
+        unordered_set<int> s(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            if(s.count(nums[i]+diff)&& s.count(nums[i]+2*diff))
+                result++;
+        }
+        return result;
+    }
+};
