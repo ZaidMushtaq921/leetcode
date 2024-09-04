5bb2962747b22a31cca50f33619f97d59ae80486
Zaid mushtaq <135120619+ZaidMushtaq921@users.noreply.github.com>
2024-09-05 00:25:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.48%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-
-        // Two hash maps to store the character mappings between s and t
-        unordered_map<char, char> s_to_t;
-        unordered_map<char, char> t_to_s;
-
-        for (int i = 0; i < s.length(); i++) {
-            char s_char = s[i];
-            char t_char = t[i];
-
-            // Check if there is a conflicting mapping in s -> t
-            if (s_to_t.count(s_char)) {
-                if (s_to_t[s_char] != t_char) return false;
-            } else {
-                s_to_t[s_char] = t_char;
-            }
-
-            // Check if there is a conflicting mapping in t -> s
-            if (t_to_s.count(t_char)) {
-                if (t_to_s[t_char] != s_char) return false;
-            } else {
-                t_to_s[t_char] = s_char;
-            }
+        int m1[256] = {0}, m2[256] = {0}, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
         }
-
-        return true;  // If no conflicts were found, the strings are isomorphic
+        return true;
     }
 };
